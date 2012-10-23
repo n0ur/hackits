692229c6d331dad38ed13d3eab04c8353acf50bb
Roobarb <hackits@in-addr.nl>
2012-10-23 20:23:52 +0200

    Removed password from repo

--- forum/Settings.php
+++ forum/Settings.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("Settings_private.php");
+
 /**
  * Simple Machines Forum (SMF)
  *
@@ -29,7 +31,6 @@ $db_type = 'mysql';
 $db_server = 'localhost';
 $db_name = 'hackits';
 $db_user = 'hackits.be';
-$db_passwd = 'Eichlosleg';
 $ssi_db_user = '';
 $ssi_db_passwd = '';
 $db_prefix = 'smf_';
